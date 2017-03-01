1cead368c198e7415c72bda9b075da9df6a11260
Rhett Herring <rhett_herring@intuit.com>
2017-02-28 19:04:58 -0700

    Added Comic book and artists models

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    // can contain attributes or behaviors.
+    public class ComicBook
+    {
+        // properties
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artists[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
