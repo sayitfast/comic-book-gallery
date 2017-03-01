db8db947b3a376514dcaecac9ec3730c40208a34
Rhett Herring <rhett_herring@intuit.com>
2017-02-28 21:11:36 -0700

    Updaed comic view detail layout

--- src/ComicBookGallery/Controllers/ComicBooks.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ComicBookGallery.Controllers
-{
-    internal class ComicBooks
-    {
-        public ComicBooks()
-        {
-        }
-    }
-}
--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -28,17 +28,17 @@ namespace ComicBookGallery.Controllers
                 }
             };
 
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = "700";
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new[]
-            {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
+            //ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            //ViewBag.IssueNumber = "700";
+            //ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            //ViewBag.Artists = new[]
+            //{
+            //    "Script: Dan Slott",
+            //    "Pencils: Humberto Ramos",
+            //    "Inks: Victor Olazaba",
+            //    "Colors: Edgar Delgado",
+            //    "Letters: Chris Eliopoulos"
+            //};
 
 
             return View(comicBook);
